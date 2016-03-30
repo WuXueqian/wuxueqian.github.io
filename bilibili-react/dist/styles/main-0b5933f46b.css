a65ed86f5698fcc9432c60365d9e5d0a1e006c22
WuXueqian <wuxueqian2010@hotmail.com>
2016-03-30 23:28:06 +0800

    Fix scripts route

--- bilibili-react/dist/styles/main-0b5933f46b.css
+++ bilibili-react/dist/styles/main-0b5933f46b.css
@@ -1,5 +1,5 @@
 /**
- * Generated on Wed Mar 30 2016 22:59:43 GMT+0800 (CST) by Wu Xueqian
+ * Generated on Wed Mar 30 2016 23:26:56 GMT+0800 (CST) by Wu Xueqian
  * @version v0.0.1
  * @link https://github.com/thundernet8/bilibili-mobile
  * @license MIT LICENSE
